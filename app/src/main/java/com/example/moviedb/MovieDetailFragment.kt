4b5f8bd8cbad2ccafe27e952658c26e49d3f67b8
Arda Türkoğlu <ardaturkoglu2909@gmail.com>
2021-04-28 22:20:59 +0300

    Navigation&layout added.
    *Navigation
    *basic layouts

---
+++ app/src/main/java/com/example/moviedb/MovieDetailFragment.kt
@@ -0,0 +1,54 @@
+package com.example.moviedb
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviedb.databinding.FragmentMovieListBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MovieDetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MovieDetailFragment : Fragment() {
+
+    private var _binding: FragmentMovieListBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private lateinit var recyclerView: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentMovieListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerView
+        binding.button.setOnClickListener{binding.recyclerView.findNavController().navigate(MovieListFragmentDirections.actionMovieListFragmentToMovieDetailFragment("movieEx"))}
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+
+}
